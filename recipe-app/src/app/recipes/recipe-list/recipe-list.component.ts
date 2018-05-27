db99dfb9dee9df6f2f570bccb8b4e0185813b80a
James Riall <james.riall@phntms.com>
2018-05-27 15:46:07 +0100

    Complete addition of services to recipe app

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { RecipeService } from './../recipe.service';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,26 +9,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe 1',
-      'Test Desc 1',
-      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/12/vegan-thai-curry.jpg?itok=8Il6uTfT'
-    ),
-    new Recipe(
-      'Test Recipe 2',
-      'Test Desc 2',
-      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/12/vegan-thai-curry.jpg?itok=8Il6uTfT'
-    )
-  ];
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe 1',
+      'Test Desc 1',
+      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/12/vegan-thai-curry.jpg?itok=8Il6uTfT',
+      [
+        new Ingredient('Tofu', 1),
+        new Ingredient('Rice', 100),
+      ]
+    ),
+    new Recipe(
+      'Test Recipe 2',
+      'Test Desc 2',
+      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/12/vegan-thai-curry.jpg?itok=8Il6uTfT',
+      [
+        new Ingredient('Vegetables', 5),
+        new Ingredient('Noodles', 100),
+      ]
+    )
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
